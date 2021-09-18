5491c4ec6f12ce15a3ae19d6edf2061cca95aca8
kawsarm104 <kawsarm104@gmail.com>
2021-09-18 18:23:24 +0600

    45.6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
 // import logo from './logo.svg';
 
+
+
+
+
+
 function App() {
+  const products = [
+    {name: 'mobile', price: 15000 },
+          {name: 'camera', price: 55000 },
+          {name: 'laptop', price: 150000 },
+          {name: 'watch', price: 5000 },
+          {name: 'shoe', price: 500 },
+          ]
+
   // creating object 
   const singer = {
   name: "Kawsar",
@@ -33,6 +46,13 @@ function App() {
       <Person name= "Park min young" job = "actress"></Person>
 
       <h1>45-4 Pass Dynamic Data To Components, Props In React</h1>
+      <Friends phone = "0125147" address="Dhaka"></Friends>
+      <Friends phone="2154788" address="Chandpur"></Friends>
+      {/* array data  */}
+      <Arraydata></Arraydata>
+          {
+            products.map(product => <Product name={product.name} price={product.price}></Product>)
+          }
     </div>
   );
 }
@@ -49,4 +69,65 @@ function Person(props) { // passing parameter
   )
 }
 
+function Friends(props) {
+  return (
+    <div className="styleee">
+      <h1>Phone: {props.phone}</h1>
+      <h3>Address: {props.address}</h3>
+    </div>
+  )
+}
+function Arraydata() {
+  const nayoks = ["re", "kfdjlds", "sdklfkldsj", "djfdlslre"]
+    const cinemas = [
+    { nayok: 'Koober', nayika: 'kopila' },
+    { nayok: 'Rubel', nayika: 'Moushumi' },
+    { nayok: 'Razzak', nayika: 'Shabana' },
+    { nayok: 'Jashim', nayika: 'Suchorita' },
+
+  ]
+  return (
+    <>
+  <h1>45-5 (Advanced) Create Multiple Components From An Array Of Objects</h1>
+    <div className="styleee">
+        <ul>
+          <li>{nayoks[0]}</li>
+          <li>{nayoks[1]}</li>
+          <li>{nayoks[2]}</li>
+        </ul>
+        {/* dynamic  */}
+        <ul>
+          {
+              nayoks.map(nayok => <li>{nayok}</li>)
+          }
+        </ul>
+          {
+        cinemas.map(cinema => <Cinema nayok={cinema.nayok} nayika={cinema.nayika}></Cinema>)
+      }
+      {/* <Product></Product> */}
+      </div>
+      </>
+  )
+}
+function Cinema(props) {
+  return (
+    <div className="styleee">
+      <h2>Nayok: {props.nayok}</h2>
+      <h4>Nayika: {props.nayika}</h4>
+    </div>
+  )
+}
+
+function Product(props) {
+  const productStyle = {
+    border: '3px solid blue',
+    borderRadius: '10px'
+  }
+  return (
+    <div className="product" style={productStyle}>
+      <h2>Name: {props.name}</h2>
+      <h4>Price: {props.price}</h4>
+    </div>
+  )
+}
 export default App;
