af2138e22b8ec2516f84f3b2ebd109ed33481a80
kawsarm104 <kawsarm104@gmail.com>
2021-09-18 11:01:10 +0600

    created person component

--- src/App.js
+++ src/App.js
@@ -8,13 +8,38 @@ function App() {
   age:"24"
   }
   // destructuring 
-  const {name, age} = singer
+  const { name, age } = singer
+  const singerStyle = {
+    backgroundColor: "purple",
+    color: "white",
+    height: "80px",
+    margin: '0 20%',
+    padding: "15px",
+    borderRadius: "12px",
+    // width:"50px"
+   
+  }
   return (
     <div className="App">
       <h1>Hello react</h1>
-      <p>Singer Name: {name} and Singer Age: { age}</p>
+
+      <p style={singerStyle}>
+       <h3>1. jsx , Dynamic Content , Dynamic Style In React </h3>
+        Singer Name: {name} and Singer Age: {age}</p>
+      <h1>45-3 Create Component, Return Html From A Component</h1>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
     </div>
   );
+}
+function Person() {
+  return (
+    <div>
+      <h1>Md. Kawsar Hossain</h1>
+    </div>
+  )
 }
 
 export default App;
