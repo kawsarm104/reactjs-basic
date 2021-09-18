96aa1537b436c471cfac88fae813a633a62212bc
kawsarm104 <kawsarm104@gmail.com>
2021-09-18 10:30:23 +0600

    created an object destructuring and passing the value dynamically

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 
 function App() {
-  
+  // creating object 
+  const singer = {
+  name: "Kawsar",
+  age:"24"
+  }
+  // destructuring 
+  const {name, age} = singer
   return (
     <div className="App">
       <h1>Hello react</h1>
+      <p>Singer Name: {name} and Singer Age: { age}</p>
     </div>
   );
 }
